b191e4c47225c28733f650e3f7d831c6d1d9b4ca
dipenitc <Dipendra.Hamal@informationtechconsultants.co.uk>
2026-04-19 15:13:02 +0100

    chore:initial commits

---
+++ src/main/java/com/dipen/k8sbasicdeploy/K8sBasicDeployApplication.java
@@ -0,0 +1,13 @@
+package com.dipen.k8sbasicdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sBasicDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sBasicDeployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dipen/k8sbasicdeploy/K8sBasicDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipen.k8sbasicdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sBasicDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
